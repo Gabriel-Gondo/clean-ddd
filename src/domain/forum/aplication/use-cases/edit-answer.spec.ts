15b3724e55e42f08fd81bf6fa212a4384e716d74
Gabriel Gondo <gabrielgondo.dev@gmail.com>
2024-02-26 08:38:54 -0300

    ✨ edit answer and question

---
+++ src/domain/forum/aplication/use-cases/edit-answer.spec.ts
@@ -0,0 +1,54 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { makeAnswer } from 'test/factories/make-answer'
+import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
+import { EditAnswerUseCase } from './edit-answer'
+
+let inMemoryAnswersRepository: InMemoryAnswersRepository
+let sut: EditAnswerUseCase
+
+describe('Edit Answer', () => {
+  beforeEach(() => {
+    inMemoryAnswersRepository = new InMemoryAnswersRepository()
+    sut = new EditAnswerUseCase(inMemoryAnswersRepository)
+  })
+
+  it('should be able to edit a answer by id', async () => {
+    const newAnswer = makeAnswer(
+      {
+        authorId: new UniqueEntityID('author-1'),
+      },
+      new UniqueEntityID('answer-1'),
+    )
+
+    await inMemoryAnswersRepository.create(newAnswer)
+
+    await sut.execute({
+      authorId: 'author-1',
+      answerId: 'answer-1',
+      content: 'edited content',
+    })
+
+    expect(inMemoryAnswersRepository.items[0]).toMatchObject({
+      content: 'edited content',
+    })
+  })
+
+  it('should be able to edit a answer by id from another user', async () => {
+    const newAnswer = makeAnswer(
+      {
+        authorId: new UniqueEntityID('author-1'),
+      },
+      new UniqueEntityID('answer-1'),
+    )
+
+    await inMemoryAnswersRepository.create(newAnswer)
+
+    expect(() => {
+      return sut.execute({
+        authorId: 'author-2',
+        answerId: 'answer-1',
+        content: 'edited content',
+      })
+    }).rejects.toBeInstanceOf(Error)
+  })
+})
